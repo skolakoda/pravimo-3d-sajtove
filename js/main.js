cb8c5d3a3effb5cb6238f2d01b21ab71fe6a18f1
Damjan Pavlica <mudroljub@gmail.com>
2016-12-24 14:10:14 +0100

    prvi komit

---
+++ js/main.js
@@ -0,0 +1,91 @@
+/*** KLASE ***/
+
+function Kamera() {
+  const temp = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000)
+  temp.position.set(0, 0, -1000)
+  return temp
+}
+
+function Kontrole() {
+  const temp = new THREE.OrbitControls(kamera)
+  temp.rotateSpeed = 1.0
+  temp.zoomSpeed = 1.2
+  temp.panSpeed = 0.8
+  return temp
+}
+
+function Geometrija() {
+  const temp = new THREE.Mesh(
+    new THREE.TorusGeometry(120, 60, 40, 40),
+    new THREE.MeshNormalMaterial()
+  )
+  temp.position.set(0, 0, 0)
+  return temp
+}
+
+function Renderer() {
+  const temp = new THREE.WebGLRenderer({ antialias: true })
+  temp.setClearColor(0xffffff, 1)
+  temp.setSize(window.innerWidth, window.innerHeight)
+  temp.domElement.style.zIndex = 5
+  return temp
+}
+
+function TekstElement() {
+  const temp = document.createElement('p')
+  temp.innerHTML = 'Plain text inside a div.'
+  temp.className = 'three-div'
+  return temp
+}
+
+function DivElement(paragraf) {
+  const temp = new THREE.CSS3DObject(paragraf)
+  temp.position.x = 0
+  temp.position.y = 0
+  temp.position.z = -185
+  temp.rotation.y = Math.PI
+  return temp
+}
+
+function Renderer2() {
+  const temp = new THREE.CSS3DRenderer()
+  temp.setSize(window.innerWidth, window.innerHeight)
+  temp.domElement.style.position = 'absolute'
+  temp.domElement.style.top = 0
+  return temp
+}
+
+/*** INSTANCE ***/
+
+const kamera = Kamera()
+const controls = Kontrole()
+const scena = new THREE.Scene()
+const geometrija = Geometrija()
+const svetlo = new THREE.HemisphereLight(0xffbf67, 0x15c6ff)
+const renderer = Renderer()
+const scena2 = new THREE.Scene()
+const paragraf = TekstElement()
+const div = DivElement(paragraf)
+const renderer2 = Renderer2()
+
+/*** FUNKCIJE ***/
+
+function init() {
+    scena.add(geometrija)
+    scena.add(svetlo)
+    document.body.appendChild(renderer.domElement)
+    scena2.add(div)
+    document.body.appendChild(renderer2.domElement)
+}
+
+function animiraj() {
+    requestAnimationFrame(animiraj)
+    renderer2.render(scena2, kamera)
+    renderer.render(scena, kamera)
+    controls.update()
+}
+
+/*** TOK ***/
+
+init()
+animiraj()
