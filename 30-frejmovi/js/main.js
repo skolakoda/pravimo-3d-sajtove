40ff3b8c4b7514af611fce03c1ac4915cd9e5253
Damjan Pavlica <mudroljub@gmail.com>
2017-04-13 16:12:10 +0200

    cistka

--- 30-frejmovi/js/main.js
+++ 30-frejmovi/js/main.js
@@ -1,113 +1,63 @@
 /* global THREE */
 
-const cssRenderer = new THREE.CSS3DRenderer()
-cssRenderer.setSize(window.innerWidth, window.innerHeight)
-cssRenderer.domElement.style.position = 'absolute'
-cssRenderer.domElement.style.top = 0
-
-const camera = new THREE.PerspectiveCamera(
-  45,
-  window.innerWidth / window.innerHeight,
-  1,
-  10000)
-camera.position.set(0, 100, 3000)
-
-// const controls = new THREE.TrackballControls(camera)
-
-document.body.appendChild(cssRenderer.domElement)
-
-const glScene = new THREE.Scene()
-const cssScene = new THREE.Scene()
-
-const ambientLight = new THREE.AmbientLight(0x555555)
-glScene.add(ambientLight)
-
-const directionalLight = new THREE.DirectionalLight(0xffffff)
-directionalLight.position.set(-.5, .5, -1.5).normalize()
-glScene.add(directionalLight)
-
-/** FUNCTIONS **/
-
-function createPlane(w, h, position, rotation) {
-  const material = new THREE.MeshBasicMaterial({
-    color: 0x000000,
-    opacity: 0.0,
-    side: THREE.DoubleSide
-  })
-  const geometry = new THREE.PlaneGeometry(w, h)
-  const mesh = new THREE.Mesh(geometry, material)
-
-  mesh.position.x = position.x
-  mesh.position.y = position.y
-  mesh.position.z = position.z
-  mesh.rotation.x = rotation.x
-  mesh.rotation.y = rotation.y
-  mesh.rotation.z = rotation.z
-
-  return mesh
+class Frejm extends THREE.CSS3DObject {
+  constructor(width, height, position, rotation, url) {
+    const iframe = document.createElement('iframe')
+    iframe.src = url
+    iframe.width = width
+    iframe.height = height
+    super(iframe)
+    this.position.x = position.x
+    this.position.y = position.y
+    this.position.z = position.z
+    this.rotation.x = rotation.x
+    this.rotation.y = rotation.y
+    this.rotation.z = rotation.z
+  }
 }
 
-function createCssObject(w, h, position, rotation, url) {
-  const html = [
-    '<div style="width:' + w + 'px; height:' + h + 'px;">',
-    '<iframe src="' + url + '" width="' + w + '" height="' + h + '">',
-    '</iframe>',
-    '</div>'
-  ].join('\n')
+const width = window.innerWidth
+const height = window.innerHeight
 
-  const div = document.createElement('div')
-  div.innerHTML = html
+const renderer = new THREE.CSS3DRenderer()
+renderer.setSize(width, height)
+renderer.domElement.style.position = 'absolute'
+renderer.domElement.style.top = 0
+document.body.appendChild(renderer.domElement)
 
-  const cssObject = new THREE.CSS3DObject(div)
-  cssObject.position.x = position.x
-  cssObject.position.y = position.y
-  cssObject.position.z = position.z
-  cssObject.rotation.x = rotation.x
-  cssObject.rotation.y = rotation.y
-  cssObject.rotation.z = rotation.z
+const kamera = new THREE.PerspectiveCamera()
+kamera.aspect = width / height
+kamera.position.set(0, 0, 2500)
 
-  return cssObject
-}
-
-function create3dPage(w, h, position, rotation, url) {
-  const plane = createPlane(
-      w, h,
-      position,
-      rotation)
-  glScene.add(plane)
-
-  const cssObject = createCssObject(
-      w, h,
-      position,
-      rotation,
-      url)
-  cssScene.add(cssObject)
-}
-
-function update() {
-  // controls.update()
-  cssRenderer.render(cssScene, camera)
-  requestAnimationFrame(update)
-}
-
-/** INIT **/
-
-create3dPage(
+const strana01 = new Frejm(
   1000, 1000,
   new THREE.Vector3(-1050, 0, 400),
   new THREE.Vector3(0, 45 * Math.PI / 180, 0),
-  'http://www.skolakoda.org/')
+  'http://www.skolakoda.org/kursevi'
+)
 
-create3dPage(
-  900, 1000,
+const strana02 = new Frejm(
+  1400, 1000,
   new THREE.Vector3(0, 0, 0),
   new THREE.Vector3(0, 0, 0),
-  'http://www.skolakoda.org/')
+  'http://www.skolakoda.org/'
+)
 
-create3dPage(
+const strana03 = new Frejm(
   1000, 1000,
   new THREE.Vector3(1050, 0, 400),
   new THREE.Vector3(0, -45 * Math.PI / 180, 0),
-  'http://www.skolakoda.org/')
+  'http://www.skolakoda.org/o-nama'
+)
+
+const scena = new THREE.Scene()
+scena.add(strana01, strana02, strana03)
+
+/** FUNCTIONS **/
+
+function update() {
+  renderer.render(scena, kamera)
+  requestAnimationFrame(update)
+}
 
 update()
