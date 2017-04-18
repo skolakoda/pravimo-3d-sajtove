eb1ddfe6a102b481fbc841c5a5c1b0211cbdd4db
Damjan Pavlica <mudroljub@gmail.com>
2017-04-18 11:39:54 +0200

    cistka

--- 40-animacija/main.js
+++ 40-animacija/main.js
@@ -1,4 +1,4 @@
-/* global THREE, TWEEN */
+/* global THREE */
 
 const width = window.innerWidth
 const height = window.innerHeight
@@ -21,27 +21,16 @@ controls.autoRotate = true
 
 function createImgObject(i) {
   const img = document.createElement('img')
-  img.src = `http://lorempixel.com/140/140?${i}`  // nasumicni query string zbog keširanja
+  img.src = `http://lorempixel.com/140/140?${i}`  // zbog keširanja
   return new THREE.CSS3DObject(img)
 }
 
-function createDomObjects(vertices) {
-	const domObjects = []
-	vertices.map((v, i) => {
-		const obj = createImgObject(i)
-		domObjects.push(obj)
-		scene.add(obj)
-	})
-	return domObjects
-}
-
-// return an array of objects with pos
 function getVertices(geometry, offset) {
   const result = []
   for (let i = 0; i < geometry.faces.length; i += 2) {
     const face = geometry.faces[i]
     const faceNext = geometry.faces[i + 1]
-	const pos = new THREE.Vector3()
+    const pos = new THREE.Vector3()
       .copy(geometry.vertices[face.a])
       .add(geometry.vertices[face.b])
       .add(geometry.vertices[face.c])
@@ -54,34 +43,20 @@ function getVertices(geometry, offset) {
   return result
 }
 
-function startTween(domObjects, vertices) {
-  const tweenIn = new TWEEN.Tween({opacity: 0})
-  .to({pos: 1.0}, 3000)
-  .onUpdate(function() {
-    const percent = this.pos
-    domObjects.forEach((obj, i) => {
-      const currentPos = vertices[i].currentPos
-      const targetPos = vertices[i].pos
-		;['x', 'y', 'z'].forEach(function (k) {
-			obj.position[k] = currentPos[k] + (targetPos[k] - currentPos[k]) * percent
-		})
-    })
-  })
-  tweenIn.start()
-}
-
 function update() {
   requestAnimationFrame(update)
   controls.update()
-  TWEEN.update()
   renderer.render(scene, camera)
 }
 
 /** EXEC **/
 
 const vertices = getVertices(new THREE.PlaneGeometry(30, 30, 8, 8), 40)
-const elements = createDomObjects(vertices)
-vertices.map((v, i) => v.currentPos = elements[i].position)	// add currentPos
 
-startTween(elements, vertices)
+vertices.map((v, i) => {
+  const imageObj = createImgObject(i)
+  imageObj.position = vertices[i].pos
+  scene.add(imageObj)
+})
+
 update()
